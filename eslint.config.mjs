c90fcfa9842b205708c94a549dacf2c9621e9063
sd44 <sd44sd44@yeah.net>
2025-01-14 17:15:23 +0800

    aa

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,42 +1,52 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import globals from 'globals'
+import pluginJs from '@eslint/js'
 // import pluginReact from "eslint-plugin-react";
 
 // https://html-eslint.org/
 // npm install --save-dev eslint @html-eslint/parser @html-eslint/eslint-plugin eslint-config-prettier
-import htmles from "@html-eslint/eslint-plugin";
-import eslintConfigPrettier from "eslint-config-prettier";
+import html from 'eslint-plugin-html'
+import htmles from '@html-eslint/eslint-plugin'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import PARSER_HTML from '@html-eslint/parser'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   eslintConfigPrettier,
   {
     // recommended configuration included in the plugin
-    ...htmles.configs["flat/recommended"],
-    files: ["**/*.{htm,html}"],
+    ...htmles.configs['flat/recommended'],
+    files: ['**/*.{htm,html}'],
+    plugins: {
+      html,
+      '@html-eslint': htmles,
+    },
+    languageOptions: {
+      parser: PARSER_HTML,
+    },
     rules: {
-      ...htmles.configs["flat/recommended"].rules, // Must be defined. If not, all recommended rules will be lost
-      "@html-eslint/indent": [
-        "warn",
+      'no-console': 2,
+      ...htmles.configs['flat/recommended'].rules, // Must be defined. If not, all recommended rules will be lost
+      '@html-eslint/indent': [
+        'warn',
         2,
         {
           Attribute: 1,
           tagChildrenIndent: {
-            html: 1,
+            html: 0,
             div: 1,
           },
         },
       ],
-      "@html-eslint/no-extra-spacing-attrs": "off",
-      "@html-eslint/require-closing-tags": "off",
-      "@html-eslint/attrs-newline": "off",
+      '@html-eslint/no-extra-spacing-attrs': 'off',
+      '@html-eslint/require-closing-tags': 'off',
+      '@html-eslint/attrs-newline': 'off',
     },
   },
   {
     // recommended configuration included in the plugin
-    files: ["**/*.{js,mjs,cjs,jsx}"],
+    files: ['**/*.{js,mjs,cjs,jsx}'],
   },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   // pluginReact.configs.flat.recommended,
-];
+]
